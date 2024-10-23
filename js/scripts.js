0c01d9bc9651742306f500a93686f1455b03219b
Yoga B <ybudhi21@gmail.com>
2024-10-23 13:14:50 +0700

    update deskripti

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@ const navLinks = document.querySelector('.nav-links');
 mobileMenu.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+
+  // Ambil input dari pengguna melalui prompt
+  let username;
+  username = window.prompt("What's your username?");
+
+  // Cek apakah pengguna memasukkan teks
+  if (username !== null && username.trim() !== "") {
+      // Tampilkan username yang dimasukkan ke dalam elemen <p> di halaman
+      document.getElementById("outputText").innerHTML = "Hai, " + username + "!" + "<br>Selamat datang di website ini...</br>";
+  } else {
+      // Jika pengguna tidak memasukkan teks atau menekan cancel
+      document.getElementById("outputText").innerHTML = "Hai Pengunjung.<br>Selamat datang di website ini... ";
+  }
